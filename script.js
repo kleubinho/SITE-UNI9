be9b1a49eec8d3a1a7f6d2061c2e308fa3240c60
Kleber Henrique <kleber.henrique@pippa.com.br>
2022-05-30 21:00:30 -0300

    Update

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const chk = document.getElementById("chk");
+const title = document.getElementById("texto");
 
 chk.addEventListener("change", () => {
   document.body.classList.toggle("dark");
+  title.classList.toggle("light");
+  // document.getElementById("texto").classList.toggle("colorWhite", e.target.checked);
+  // title.style.color = ("white");
 });
 
 function teste(value) {
