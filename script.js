b0b767e8b94927265854f979b2961cd90728cb29
Kleber Henrique <kleber.henrique@pippa.com.br>
2022-06-08 20:36:08 -0300

    Update

--- script.js
+++ script.js
@@ -4,14 +4,11 @@ const title = document.getElementById("texto");
 chk.addEventListener("change", () => {
   document.body.classList.toggle("dark");
   title.classList.toggle("light");
-  // document.getElementById("texto").classList.toggle("colorWhite", e.target.checked);
-  // title.style.color = ("white");
 });
 
 function teste(value) {
   if (value == "sobre") {
       sobre.classList.toggle("testeDeCor");
-    console.log("mudei");
   } else if (value == "funciona") {
     funciona.classList.toggle("testeDeCor");
   } else if (value == "projetos") projetos.classList.toggle("testeDeCor");
